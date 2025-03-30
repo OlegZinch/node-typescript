86135da6747d0684c8da521e4ded5ff095ebc676
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-03-31 00:51:22 +0100

    added simple server and  @types/node

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!')
+import { createServer } from 'node:http'
+
+const server = createServer((req, res) => {
+  console.log(`Request method: ${req.method}, URL: ${req.url}`)
+  res.end('Hello, world!')
+})
+
+server.listen(3000, () => {
+  console.log('Server is running at http://localhost:3000')
+})
