df22f6a821a46ba8d37c3e3f5dbee8f4eb32529f
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-03-31 14:46:13 +0100

    finished the basic app

--- routes/todo.ts
+++ routes/todo.ts
@@ -1,5 +1,5 @@
-import express, { Request, Response } from 'express'
-import { addTodo } from '../data.js'
+import express from 'express'
+import { addTodo, getTodos, getTodo, updateTodo, removeTodo } from '../data.js'
 
 const router = express.Router()
 
@@ -12,6 +12,26 @@ router.post('/todos', (req, res) => {
   const addedTodo = addTodo(text)
 
   res.json({ message: 'Todo added!', todo: addedTodo })
+})
+
+router.get('/todos', (req, res) => {
+  const todos = getTodos()
+  res.json({ todos })
+})
+
+router.get('/todos/:id', (req, res) => {
+  const todo = getTodo(+req.params.id)
+  res.json({ todo })
+})
+
+router.patch('/todos/:id', (req, res) => {
+  const updatedTodo = updateTodo(+req.params.id, req.body.text)
+  res.json({ message: 'Todo updated!', todo: updatedTodo })
+})
+
+router.delete('/todos/:id', (req, res) => {
+  removeTodo(+req.params.id)
+  res.json({ message: 'Todo deleted!' })
 })
 
 export default router
